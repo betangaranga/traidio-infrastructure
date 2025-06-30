45b1b1049c317bbff8c9e2deb8be14519e7752f2
Angel Aguilera <albertoangelaguilera@gmail.com>
2025-06-29 21:39:55 -0600

    fixed posgres version

--- infra_stack.py
+++ infra_stack.py
@@ -28,7 +28,9 @@ class InfraStack(Stack):
         # ✅ Aurora Serverless v2 Postgres
         db_cluster = rds.ServerlessCluster(
             self, f"traidio-aurora-{env_type}",
-            engine=rds.DatabaseClusterEngine.AURORA_POSTGRESQL,
+            engine=rds.DatabaseClusterEngine.aurora_postgres(
+                version=rds.AuroraPostgresEngineVersion.VER_15_7
+            ),
             vpc=vpc,
             scaling=rds.ServerlessScalingOptions(
                 auto_pause=cdk.Duration.minutes(10),
